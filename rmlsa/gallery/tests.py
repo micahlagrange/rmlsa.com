92ece5b772f17f332dc32e789305ad1cbd4d3ea5
Micah Turner <micah@lagrangesystems.com>
2015-10-17 16:26:52 -0600

    Added datetime naive test in gallery app

--- rmlsa/gallery/tests.py
+++ rmlsa/gallery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rmlsa.gallery import GalleryImage
 
 # Create your tests here.
+
+class DateTimeTestCase(TestCase):
+	def gallery_image_test(self):
+		date = datetime.datetime.now()
+		self.assertEqual(date.tzinfo, not None)
+		self.assertEqual(date.tzinfo.utcoffset(date), not None)
+
