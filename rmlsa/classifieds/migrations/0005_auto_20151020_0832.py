421ca9e374ed63f6ba4456aeff3ad3fde5a5ea83
Micah Turner <micah@lagrangesystems.com>
2015-10-20 08:36:33 -0600

    increased max digits of ad price (including cents) to 10.

---
+++ rmlsa/classifieds/migrations/0005_auto_20151020_0832.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('classifieds', '0004_auto_20151020_0828'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='ad',
+            name='price',
+            field=models.DecimalField(default=0, null=True, decimal_places=2, max_digits=10),
+        ),
+    ]
