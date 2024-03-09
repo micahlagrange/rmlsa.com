92d4509756d9e9e251c8bb4ff0fe43d30aab98e6
Micah Turner <micah@lagrangesystems.com>
2024-03-08 23:40:30 -0700

    cascade them deletes... oh and ig we're upgrading to django 4 shrug

--- rmlsa/home/models.py
+++ rmlsa/home/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class WelcomeMessage(models.Model):
     text = models.TextField(null=True, blank=False)
-    date_created = models.DateField(verbose_name="Date created", auto_now=True, blank=False)
-    date_modified = models.DateField(verbose_name="Date edited", auto_now=True, blank=False)
+    date_created = models.DateField(
+        verbose_name="Date created", auto_now=True, blank=False)
+    date_modified = models.DateField(
+        verbose_name="Date edited", auto_now=True, blank=False)
 
     def __str__(self):
         return '{}'.format(self.text[0:50])
@@ -30,21 +32,25 @@ class LinkCategory(models.Model):
 class WebLink(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(null=True, blank=False)
-    category = models.ForeignKey(LinkCategory, null=True, blank=False)
+    category = models.ForeignKey(
+        LinkCategory, on_delete=models.CASCADE, null=True, blank=False)
 
     def __str__(self):
         return '{}'.format(self.name)
+
 
 class MembershipApplication(models.Model):
     name = models.CharField(max_length=200)
-    application_file = models.FileField(upload_to='forms', null=True, blank=False)
+    application_file = models.FileField(
+        upload_to='forms', null=True, blank=False)
 
     def __str__(self):
         return '{}'.format(self.name)
+
 
 class RulesFile(models.Model):
     name = models.CharField(max_length=200)
     rules_file = models.FileField(upload_to='forms', null=True, blank=False)
-   
+
     def __str__(self):
         return '{}'.format(self.name)
