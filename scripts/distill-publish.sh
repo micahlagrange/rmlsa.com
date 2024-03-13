43ea053c422eff9598cfa8dfb63b348cfd4fa06f
Micah turner <turner.micah@gmail.com>
2024-03-13 06:22:19 +0000

    add urls for convenience after publish script

--- scripts/distill-publish.sh
+++ scripts/distill-publish.sh
@@ -9,3 +9,6 @@ set -e
 
 aws s3 sync /opt/rmlsa/build/ s3://distill-staging/
 
+echo "visit the page: http://distill-staging.s3-website-us-east-1.amazonaws.com"
+echo "Staging URL: https://dvuh0qlj8uj57.cloudfront.net"
+
