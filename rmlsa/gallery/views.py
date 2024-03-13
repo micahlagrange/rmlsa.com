1890d5e852fe965c82dd3cc353923e5f1b7430c4
Micah Turner <turner.micah@gmail.com>
2024-03-12 23:56:01 -0600

    pic archive static

--- rmlsa/gallery/views.py
+++ rmlsa/gallery/views.py
@@ -15,28 +15,26 @@ def gallery_pictures(request):
                                                              'partners': home.views.get_partner_links()})
 
 
-def video_archive(request, year):
-    if year in (2012, 2013, 2014):
-        return render(request, 'gallery/video_archive/{}_videos.html'.format(year),
-                      {'videos': 'active', 'random_image': home.views.get_random_image(),
-                       'partners': home.views.get_partner_links()})
-    else:
-        return HttpResponseRedirect(reverse('gallery:video_archive_index'))
-
-
 def video_archive_index(request):
     return render(request, 'gallery/video_archive/index.html', {'videos': 'active',
                                                                 'random_image': home.views.get_random_image(),
                                                                 'partners': home.views.get_partner_links()})
 
 
-def picture_archive(request, year):
-    if year in (2011, 2012, 2013):
-        return render(request, 'gallery/picture_archive/{} RMLSA Gallery.html'.format(year),
-                      {'gallery': 'active', 'random_image': home.views.get_random_image(),
-                       'partners': home.views.get_partner_links()})
-    else:
-        return HttpResponseRedirect(reverse('gallery:picture_archive_index'))
+def picture_archive_for_year(request, year):
+    return render(request, 'gallery/picture_archive/{} RMLSA Gallery.html'.format(year),
+                  {'gallery': 'active', 'random_image': home.views.get_random_image(),
+                   'partners': home.views.get_partner_links()})
+
+
+def picture_archive_2011(request):
+    return picture_archive_for_year(request, 2011)
+
+def picture_archive_2012(request):
+    return picture_archive_for_year(request, 2012)
+
+def picture_archive_2013(request):
+    return picture_archive_for_year(request, 2013)
 
 
 def picture_archive_index(request):
