d3974531f677eba6ead570fe8cc76ffc09e1efb6
Micah Turner <turner.micah@gmail.com>
2024-03-12 22:52:37 -0600

    no forms no login no delete or uploads

--- rmlsa/gallery/views.py
+++ rmlsa/gallery/views.py
@@ -8,66 +8,6 @@ from gallery.models import GalleryImage
 import home.views
 
 
-@login_required
-def upload_image(request):
-    custom_errors = []
-
-    if request.method == 'POST':
-        if request.user.has_perm('gallery.upload_image'):
-            form = forms.GalleryImageUploader(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-                filename = form.cleaned_data['name']
-                return HttpResponseRedirect(reverse('gallery:upload_success', kwargs={'filename': filename}))
-        else:
-            custom_errors.append(
-                'You do not have permission to upload images. Please contact the site administrator')
-            return render(request, 'gallery/upload_image_form.html', {'custom_errors': custom_errors})
-    else:
-        if request.user.has_perm('gallery.upload_image'):
-            form = forms.GalleryImageUploader()
-        else:
-            form = None
-            custom_errors.append(
-                'You do not have permission to upload images. Please contact the site administrator')
-
-    return render(request, 'gallery/upload_image_form.html', {'form': form, 'custom_errors': custom_errors})
-
-
-def delete_success(request):
-    custom_success = ['The image has been deleted successfully']
-    return render(request, 'gallery/delete_success.html', {'custom_success': custom_success})
-
-
-def upload_success(request, filename):
-    custom_success = ['{} has been uploaded successfully'.format(filename)]
-    context = home.views.base_tc('gallery')
-    context.update({'custom_success': custom_success})
-    return render(request, 'gallery/upload_success.html', context)
-
-
-def delete_image(request, pk):
-    image = GalleryImage.objects.get(pk=pk)
-    custom_errors = []
-
-    if request.method == 'POST' and 'is_submitted' in request.POST:
-        if request.user.has_perm('gallery.delete_image'):
-            form = forms.DeleteModel(request.POST)
-            if form.is_valid():
-                image.delete()
-                return HttpResponseRedirect(reverse('gallery:delete_success'))
-            else:
-                raise ValueError(form)
-        else:
-            custom_errors.append(
-                'You do not have permission to edit this object.')
-            return render(request, 'gallery/delete_confirmation.html', {'custom_errors': custom_errors})
-    else:
-        form = forms.DeleteModel(initial={'is_submitted': '1'})
-
-    return render(request, 'gallery/delete_confirmation.html', {'image': image, 'form': form})
-
-
 def gallery_pictures(request):
     all_images = GalleryImage.objects.all()
     return render(request, 'gallery/gallery_pictures.html', {'gallery': 'active', 'images': all_images,
