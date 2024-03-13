6fe82145317fe8b8042ddb5a8465fe056f27f532
Micah Turner <turner.micah@gmail.com>
2024-03-12 23:41:38 -0600

    delete comments

--- rmlsa/forms.py
+++ rmlsa/forms.py
@@ -3,28 +3,5 @@ from gallery.models import GalleryImage
 from home.models import PartnerSite
 
 
-# class GalleryImageUploader(forms.ModelForm):
-#     class Meta:
-#         model = GalleryImage
-#         fields = ['image', 'name', 'caption', 'date']
-#         permissions = (
-#             ('upload_image', 'Can upload images'),
-#             ('delete_image', 'Can delete images'),
-#         )
-
-
-# class PartnerEdit(forms.ModelForm):
-#     class Meta:
-#         model = PartnerSite
-#         fields = ['name', 'image', 'url']
-#         permissions = (
-#             ('edit_partner', 'Can edit partner links'),
-#         )
-
-
-# class DeleteModel(forms.Form):
-#     is_submitted = forms.CharField(widget=forms.HiddenInput())
-
-
 class SearchForm(forms.Form):
     query_string = forms.Textarea
