4de852a1e985a1a2b5465874eeebea9a9e4f2612
Micah Turner <turner.micah@gmail.com>
2024-05-18 14:37:38 -0600

    move static css to home app

--- rmlsa/rmlsa/settings.py
+++ rmlsa/rmlsa/settings.py
@@ -64,6 +64,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 ALLOWED_HOSTS = [
     'admin.rmlsa.com',
