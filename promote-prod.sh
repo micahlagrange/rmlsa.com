09c3df897268505100ca729a1dfd7987fcbb4d94
Micah Turner <turner.micah@gmail.com>
2024-05-18 13:55:27 -0600

    promote profile

--- promote-prod.sh
+++ promote-prod.sh
@@ -1,7 +1,7 @@
-aws --prod \
+aws --profile prod \
 s3 sync s3://distill-staging/ s3://rmlsa-prod/
 
-aws --prod \
+aws --profile prod \
 cloudfront create-invalidation \
 --distribution-id $(cat /etc/rmlsa.com.conf/prod_distrib) \
 --paths '/*'
